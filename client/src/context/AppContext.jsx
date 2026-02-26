aff581af68d2e8254dd11e16e0bd721001f781fb
keshav62 <keshavsing1230@gmail.com>
2026-02-27 03:22:45 +0530

    add context and routes

---
+++ client/src/context/AppContext.jsx
@@ -0,0 +1,46 @@
+import { useContext, createContext, useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { dummyChats, dummyUserData } from "../assets/assets";
+
+let AppContext = createContext(); 
+
+export const AppContextProvider = ({children}) => { 
+
+  const navigate = useNavigate(); 
+  const [user, setUser] = useState(null); 
+  const [chats, setChats] = useState([]); 
+  const [selectedChat, setSelectedChat ] = useState(null); 
+  const [theme, setTheme] = useState(localStorage.getItem ('theme') || ('light')); 
+
+  const fetchUser = async()=> { 
+    setUser(dummyUserData)
+  }
+
+  const fetchUsersChats = async()=>{ 
+    setChats(dummyChats);
+    setSelectedChat(dummyChats[0]);
+  }
+
+  useEffect(()=> { 
+    if(theme == 'dark') { 
+      document.documentElement.classList.add("dark"); 
+    }
+    else { 
+      document.documentElement.classList.remove("dark"); 
+    }
+  },[theme])
+
+  useEffect(()=> { 
+    fetchUser(); 
+  },[])
+  
+  const value = {navigate, user, setUser, chats, setChats, selectedChat, setSelectedChat, theme}
+
+  return (
+    <AppContext.Provider value={value}> 
+      {children}
+    </AppContext.Provider>
+  )
+}
+
+export const useAppContext = () => useContext(AppContext); 
---
+++ client/src/pages/Community.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Community() {
+  return (
+    <div>
+      <p>community</p>
+    </div>
+  )
+}
+
+export default Community

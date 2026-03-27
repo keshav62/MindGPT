bfe198dfdf9b01391684daa29c615a0d9bf68d7e
keshav62 <keshavsing1230@gmail.com>
2026-03-28 01:19:34 +0530

    completed community  and credis page

--- client/src/pages/Community.jsx
+++ client/src/pages/Community.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import  { useEffect, useState } from 'react'
+import { dummyPublishedImages } from '../assets/assets';
+import Loading from './Loading';
 
 function Community() {
+
+  const [images, setImages] = useState([]); 
+  const [loading, setLoading] = useState(true); 
+
+  const fetchImage = async ()=> { 
+    setImages(dummyPublishedImages); 
+    setLoading(false); 
+  }
+
+  useEffect(()=> { 
+    fetchImage(); 
+  },[]); 
+
+  if(loading) return <Loading/> 
+
   return (
-    <div>
-      <p>community</p>
+    <div className='p-6 pt-12 xl:px-12 2xl:px-20 w-full mx-auto h-full overflow-y-scroll '>
+      <h2 className='text-xl font-semibold mb-6 text-gray-800 dark:text-purple-100 '>Community Images</h2>
+
+      {images.length > 0 ? (
+        <div className='flex flex-wrap max-sm:justify-center gap-5'>
+          {images.map((item,index) => (
+            <a key={index} href={item.imageUrl} target='_blank' className='relative group block rounded-lg overflow-hidden border border-gray-200 dark:border-purple-700 shadow-sm hover:shadow-md duration-300 '>
+              <img src={item.imageUrl} alt="" className='w-full h-40 md:h-50 2xl:h-62 object-cover group-hover:scale-105 transition-transform duration-300 ease-in-out '/>
+              <p className='absolue bottom-0 right-0 text-xs bg-black/50 backdrop-blur text-white px-4 py-1 rounded-tl-xl opacity-0 group-hover:opacity-100 transition duration-100 '>Created by {item.userName}</p>
+            </a>
+          ))}
+        </div>
+      ) : 
+      (
+        <p className='text-center text-gray-600 dark:text-purple-200 mt-10'>No Images Available.</p>
+      )}
     </div>
   )
 }
